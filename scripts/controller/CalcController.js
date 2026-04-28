bd364e6f9fb7d58faa9a124453ed7e5cf3d66342
jaumrosa <joao.rosa5@outlook.com>
2026-04-27 23:19:38 -0300

    C15 - Implementação do botão ponto e refatoração do botão AC

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -30,6 +30,8 @@ class CalcController  {
 
     clearAll(){
         this._operation = [];
+        this._lastNumber = '';
+        this._lastOperator = '';
         this.setLastNumberToDisplay();
     }
 
@@ -139,10 +141,6 @@ class CalcController  {
 
                 this.setLastOperation(value);
 
-            } else if(isNaN(value)){
-                
-                console.log('outra coisa' , value);
-
             } else{
 
                 this.pushOperation(value);
@@ -159,7 +157,7 @@ class CalcController  {
             } else {
 
                 let newValue = this.getLastOperation().toString() + value.toString();  
-                this.setLastOperation(parseInt(newValue));
+                this.setLastOperation(parseFloat(newValue));
 
                 this.setLastNumberToDisplay();
 
@@ -170,6 +168,19 @@ class CalcController  {
 
     setError(){
         this.displayCalc = "Error";
+    }
+
+    addDot(){
+
+        let lastOperation = this.getLastOperation();
+
+        if(this.isOperator(lastOperation) || !lastOperation){
+            this.pushOperation('0.');
+        } else {
+            this.setLastOperation(lastOperation.toString() + '.');
+        }
+        this.setLastNumberToDisplay();
+
     }
 
     execBtn(value){
@@ -207,7 +218,7 @@ class CalcController  {
                 break;
 
             case 'ponto':
-                this.addOperation('.');
+                this.addDot();
                 break;
 
             case '0':
