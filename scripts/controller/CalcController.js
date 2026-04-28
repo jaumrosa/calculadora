944453ec983a6e9cca188f5c76c43785e58b9bbf
jaumrosa <joao.rosa5@outlook.com>
2026-04-27 22:14:55 -0300

    C12 - Criação de método para atualizar último número inserido ao display

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -63,21 +63,32 @@ class CalcController  {
     calc() {
 
         let last = this._operation.pop();
-
         let result = eval(this._operation.join(""));
-
+        
         this._operation = [result, last];
+        this.setLastNumberToDisplay();
         
     }
 
     setLastNumberToDisplay(){
 
-        
+         let lastNumber;
+
+         for(let i = this._operation.length - 1; i >=0; i--){
+
+            if(!this.isOperator(this._operation[i])){
+                lastNumber = this._operation[i];
+                break;
+            }
+         }
+
+         this.displayCalc = lastNumber;
 
 
     }
 
     addOperation(value){
+
         if(isNaN(this.getLastOperation())){
             
             if(this.isOperator(value)) {
@@ -91,6 +102,7 @@ class CalcController  {
             } else{
 
                 this.pushOperation(value);
+                this.setLastNumberToDisplay();
             }
 
 
