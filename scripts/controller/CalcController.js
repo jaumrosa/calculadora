70d950efcd3d79cadc2cc6551bbd97d977be24f0
jaumrosa <joao.rosa5@outlook.com>
2026-04-27 22:03:39 -0300

    C11 - Agrupando operações em pares com pop() e calculando resultando usando eval()

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -21,7 +21,6 @@ class CalcController  {
 
     addEventListenerAll(element, events, fn){
         events.split(' ').forEach(event => {
-
             element.addEventListener(event, fn, false);
         });
     }
@@ -50,6 +49,34 @@ class CalcController  {
         
     }
 
+    pushOperation(value){
+
+        this._operation.push(value);
+
+        if(this._operation.length > 3) {
+
+            this.calc();
+
+        }
+    }
+
+    calc() {
+
+        let last = this._operation.pop();
+
+        let result = eval(this._operation.join(""));
+
+        this._operation = [result, last];
+        
+    }
+
+    setLastNumberToDisplay(){
+
+        
+
+
+    }
+
     addOperation(value){
         if(isNaN(this.getLastOperation())){
             
@@ -59,20 +86,30 @@ class CalcController  {
 
             } else if(isNaN(value)){
                 
-                console.log(value);
+                console.log('outra coisa' , value);
 
             } else{
 
-                this._operation.push(value);
+                this.pushOperation(value);
             }
 
 
         } else{
-            let newValue = this.getLastOperation().toString() + value.toString();  
-            this.setLastOperation(parseInt(newValue));
+
+            if(this.isOperator(value)){
+
+                this.pushOperation(value);
+
+            } else {
+
+                let newValue = this.getLastOperation().toString() + value.toString();  
+                this.setLastOperation(parseInt(newValue));
+
+                this.setLastNumberToDisplay();
+
+            }
         }
         
-        console.log(this._operation);
     }
 
     setError(){
